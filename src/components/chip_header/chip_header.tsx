ba7ab5c8b3c751bb028f352378ed7c04e8c68a16
Chris Jackem <chrisjackem@gmail.com>
2025-09-06 13:28:58 -0400

    Footer and chipHeaders

---
+++ src/components/chip_header/chip_header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './chip_header.css';
+
+type ChipHeaderProps = {
+    title: string;
+    colBg?: string;
+    colTx?: string;
+    children?: React.ReactNode;
+};
+
+const ChipHeader = ({ title, colBg, colTx, children }: ChipHeaderProps) => {
+    const background_color = colBg ? colBg : `var(--background, #999)`;
+    const text_color = colTx ? colTx : `var(--foreground, #FFF)`;
+    return (
+        <section className=''>
+        <div 
+            className='chipped-header'
+            style={{ borderBottom: `4px solid ${background_color}` }}
+        >
+            <h3 
+                className='chip-text chip-tl-md-npad'
+                style={{ backgroundColor: background_color, color: `${text_color}`}}
+            >{title}</h3>
+        </div>
+        { children && <div className='chip-header-child-container padded'>{ children }</div>}
+        </section>
+    );
+}
+export default ChipHeader
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import './footer.css'
+import ChipHeader from '../chip_header/chip_header'
 
 const Footer = () => {
   return (
     <footer id='footer-container'>
       
-      <div id='footer-header' className='chip-tl-md'>
-        <h3>chrisjackem.com</h3>
+      <div id='footer-header'>
+        <h2>Footer</h2>
+        <br/>
+        <ChipHeader 
+        title='Links'
+        colBg='#FFF'
+        colTx='#000'
+        />
         <a href='/home'>Home</a>
         <br/>
         <a href='/about'>Home</a>      
       </div>
 
-      <div id='footer-aside'>
+      <div id='footer-aside' className='flex-column chip-tl-md tx-fg'>
+        
+        <ChipHeader title='Links'/>
+        {/* <ChipHeader 
+        title='Test'
+        colBg='red'
+        /> */}
         <h3>Stuff</h3>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, error?</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, error?</p>
@@ -22,7 +35,7 @@ const Footer = () => {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet, error?</p>
       </div>
 
-      <div id='footer-footer'>
+      <div id='footer-footer' className='chip-tl-md'>
         <h4>Bla</h4>
         <p>BLAH</p>
         <p>BLAH</p>
