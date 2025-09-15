471e21647ff4567a7de6cb33e81b0cc4ef739188
Chris Jackem <chrisjackem@gmail.com>
2025-09-15 10:48:53 -0400

    Back and forth buttons 2

--- src/components/scroller/scroller.tsx
+++ src/components/scroller/scroller.tsx
@@ -9,7 +9,6 @@ import { motion } from 'motion/react';
 
 const TIMEOUT = 200;
 const MIN_DISTANCE = 300;
-
 const COLOR_ACCENT = 'var(--accent, #FFF)';
 const COLOR_MIDGROUND = 'var(--midground, #CCC)';
 const COLOR_DARKEST = 'var(--darkest, #000)';
@@ -17,8 +16,7 @@ const COLOR_DARKEST = 'var(--darkest, #000)';
 const Scroller: React.FC<React.PropsWithChildren<object>> = ({ children }) => {    
     const timer = useRef<number | null>(null);
     const {refsByKey, setRef} = useRefs();
-    const [nextState, setNextState] = useState(true);
-    const [scrollState, setScrollState] = useState(true);
+    const [scrollState, setScrollState] = useState('first');
 
     // Listener
     useEffect(()=>{        
@@ -29,15 +27,15 @@ const Scroller: React.FC<React.PropsWithChildren<object>> = ({ children }) => {
     function scrollHandler(){        
         if ( timer.current ){       
             window.clearTimeout(timer.current)
-            /* setBtnNextColor()  */
         }
         timer.current =  window.setTimeout(()=>{
             scrollSnap();
+            if (window.scrollY === 0) setScrollState('first');
             timer.current = null;
         }, TIMEOUT);
     }
 
-    // Snappin'
+    // Called from timeou. Scrolls to closest refByKey
     function scrollSnap(){
         for( const element of Object.values(refsByKey)){
             if (!element) continue;
@@ -48,51 +46,40 @@ const Scroller: React.FC<React.PropsWithChildren<object>> = ({ children }) => {
         }
     }
 
-/*     function toggleScroller(){
-        setScrollState( scrollState => !scrollState );
-    } */
-
     function scrollNext(reverse=false){
         const last_element = (Object.keys(refsByKey).length - 1).toString();  
         const obj = reverse ? Object.entries(refsByKey).reverse() : Object.entries(refsByKey);  
         for( const [key, element] of obj ){
             if (!element) continue;
             const rect_y = element.getBoundingClientRect().y;
+            // Different checks for reverse - this allows both btns to use this
             if ( !reverse && rect_y > 0 || reverse && rect_y < 0 ){
                 window.scrollBy( { top: rect_y, behavior: 'smooth'});
-                setNextState(key.toString() !== last_element);
+                //setNextState(key.toString() !== last_element);
+                if (key === last_element){
+                    setScrollState('last');
+                }else  if (key === '0'){
+                    setScrollState('first');
+                }else{
+                    setScrollState('');
+                }
                 return;
             }
         }
     }
-    /* function scrollNext(reverse=false){
-        const last_element = (Object.keys(refsByKey).length - 1).toString();  
-        const obj = reverse ? Object.entries(refsByKey).reverse() : Object.entries(refsByKey);  
-        for( const [key, element] of obj ){
-            if (!element) continue;
-            const rect_y = element.getBoundingClientRect().y;
-            if (rect_y > 0){
-                window.scrollBy( { top: rect_y, behavior: 'smooth'});
-                setNextState(key.toString() !== last_element);
-                return;
-            }
-        }
-    } */
-    
+        
     return (
         <>
-            <motion.div className={`flex ${styles.scroll_tools}`}>
-                <BtnScroll 
-                    onClick={() => setScrollState( scrollState => !scrollState )} 
-                    color={scrollState ? COLOR_ACCENT : COLOR_MIDGROUND} 
-                />                 
-                <BtnPrev
+            <motion.div className={`flex ${styles.scroll_tools}`}>                          
+                <SvgBtn type={'prev'}
                     onClick={()=>scrollNext(true)} 
-                    color={nextState ? COLOR_ACCENT : COLOR_DARKEST} 
+                    color={scrollState !== 'first' ? COLOR_ACCENT : COLOR_DARKEST}
+                    /* disabled={scrollState !== 'first'} */
                 />
-                <BtnNext 
+                <SvgBtn type={'next'}
                     onClick={()=>scrollNext()} 
-                    color={nextState ? COLOR_ACCENT : COLOR_DARKEST} 
+                    color={scrollState !== 'last' ? COLOR_ACCENT : COLOR_DARKEST}
+                    /* disabled={scrollState !== 'last'} */
                 />
             </motion.div>
 
@@ -108,50 +95,33 @@ const Scroller: React.FC<React.PropsWithChildren<object>> = ({ children }) => {
     )
 }
 
-export const BtnScroll = ({ color, onClick, disabled=false }:{color?: string, disabled?:boolean, onClick:(e: React.MouseEvent<HTMLButtonElement>) => void }) => {
+export const SvgBtn = ({ color, onClick, type, disabled = false }: { color?: string; type:'next'|'prev'; disabled?: boolean; onClick: (e: React.MouseEvent<HTMLButtonElement>) => void }) => {
     const final_color = color ? color : 'var(--accent, #FFF)';
-    return (
-        <button 
-            className={`${styles.btn_scroll}`} 
-            onClick={onClick}
-            disabled={disabled}
-        >
-           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">  
-                <polygon fill={final_color} stroke-width="4%" stroke="#000" points="99.25 37 137.5 37 137.5 187 99.25 187 99.25 149.5 62.5 149.5 62.5 112 99.25 37"/>
-                <rect fill={final_color} stroke-width="3px" stroke="#000" x="62" y="12.5" width="76" height="12.5"/>  
-            </svg> 
-        </button>
-    )
-}
-
-export const BtnNext = ({ color, onClick, disabled = false }: { color?: string; disabled?: boolean; onClick: (e: React.MouseEvent<HTMLButtonElement>) => void }) => {
-    const final_color = color ? color : 'var(--accent, #FFF)';
+    function getSvg(){
+        switch(type){
+            case 'next': return (<SVG_NEXT final_color={color} />)
+            case 'prev': return (<SVG_PREV final_color={color} />)
+        }
+    }
     return (
         <button
             onClick={onClick}
             className={`${styles.btn_next}`}
             disabled={disabled}
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-                <polygon fill={final_color} stroke-width="4%" stroke="#000" points="176 137.24 176 175.75 25 175.75 25 137.24 62.75 137.24 61.75 88 99.5 88 176 137.24" />
-            </svg>
-        </button>
+        >{ getSvg() }</button>
     );
 };
 
-export const BtnPrev = ({ color, onClick, disabled = false }: { color?: string; disabled?: boolean; onClick: (e: React.MouseEvent<HTMLButtonElement>) => void }) => {
-    const final_color = color ? color : 'var(--accent, #FFF)';
-    return (
-        <button
-            onClick={onClick}
-            className={`${styles.btn_next}`}
-            disabled={disabled}
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-                <polygon fill={final_color} stroke-width="4%" stroke="#000" points="25 137.24 25 175.75 176 175.75 176 137.24 138.25 137.24 139.25 88 101.5 88 25 137.24"/>
-            </svg>
-        </button>
-    );
-};
+export const SVG_NEXT = ({ final_color }: { final_color: string | undefined }) => (
+    <svg viewBox="0 0 200 200">
+        <polygon fill={final_color} strokeWidth="4%" stroke="#000" points="176 137.24 176 175.75 25 175.75 25 137.24 62.75 137.24 61.75 88 99.5 88 176 137.24" />
+    </svg>
+);
+export const SVG_PREV = ({ final_color }: { final_color: string | undefined }) => (
+    <svg viewBox="0 0 200 200">
+        <polygon fill={final_color} strokeWidth="4%" stroke="#000" points="25 137.24 25 175.75 176 175.75 176 137.24 138.25 137.24 139.25 88 101.5 88 25 137.24"/>
+    </svg>
+);
+
 
 export default Scroller;
