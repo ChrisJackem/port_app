d69dc9e1423c3a03c377b0d0c1fa444adbd07b61
Chris Jackem <chrisjackem@gmail.com>
2026-05-04 20:59:53 -0400

    contact form optimize test

--- src/components/contact_form/contact_form.tsx
+++ src/components/contact_form/contact_form.tsx
@@ -151,9 +151,9 @@ const ContactForm = () => {
                 <motion.div 
                     key={'contact'} 
                     className={`modal ${styles.container}`}
-                    initial={{ opacity: 0, x: 100, scale: 0.9 }}
-                    animate={{ opacity: 1, x: 0, scale: 1 }}
-                    exit={{ opacity: 0, x: 100, scale: 0.9 }}
+                    initial={{ opacity: 0, y: 10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 10 }}
                     transition={{ duration: 0.2, ease: easeOut }}
                 >
                     <div className={`flex ${styles.top_strip}`}>
